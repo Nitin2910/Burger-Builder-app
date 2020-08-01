07dc8d996e2e8d82fc8a124b1ef7fa0b00fc48d7
Nitin Johri <nitin.johri@ad.infosys.com>
2020-08-01 15:57:45 +0530

    aux

---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Aux from '../../Hock/Auxi';
+import LayoutCss from './Layout.css';
+const layout = (props) => (
+    <Aux>
+        <div>Tool bar , Side Drawer , Backdrop</div>
+        <main className={LayoutCss}>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
---
+++ src/Container/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+import Aux from '../../Hock/Auxi';
+
+class BurgerBUilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+
+}
+
+export default BurgerBUilder;
